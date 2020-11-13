35fce54317279a5b3f4c5cb6d1eeaac685792a8e
Marcu Alexandru-Daniel <marcudanielalexandru@gmail.com>
2020-11-13 22:22:01 +0200

    ...

--- vrpAPI/src/main/java/marcu/rwtdvrpapi/api/adapter/VrpToProviderAdapter.java
+++
@@ -1,9 +0,0 @@
-package marcu.rwtdvrpapi.api.adapter;
-
-import marcu.rwtdvrpapi.api.domain.Route;
-
-import java.time.Instant;
-
-public interface VrpToProviderAdapter{
-    Route evaluate(Route route, Instant departureTime);
-}
--- vrpAPI/src/main/java/marcu/rwtdvrpapi/api/service/RWTDVRPService.java
+++
@@ -1,9 +0,0 @@
-package marcu.rwtdvrpapi.api.service;
-
-import marcu.rwtdvrpapi.api.domain.Solution;
-import marcu.rwtdvrpapi.api.domain.VRPInstance;
-
-public interface RWTDVRPService {
-    String start(VRPInstance vrpInstance) throws Exception;
-    Solution stop(String id) throws Exception;
-}
